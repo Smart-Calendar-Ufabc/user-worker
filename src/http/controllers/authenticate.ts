c86e84bc06c11fc3124a21464b44989a9657f68a
Giselle Hoekveld <gisellehoekveld@gmail.com>
2024-04-05 00:21:18 -0300

    Refactor authentication controller

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,34 +1,41 @@
-import { Context } from "hono";
-import { z } from "zod";
-import { makeAuthenticateUseCase } from "../../use-cases/factories/make-authenticate";
-import { InvalidCredentialsError } from "../../use-cases/errors/InvalidCredentialsError";
+import { Context } from 'hono'
+import { z } from 'zod'
+import { makeAuthenticateUseCase } from '../../use-cases/factories/make-authenticate'
+import { InvalidCredentialsError } from '../../use-cases/errors/InvalidCredentialsError'
 
 export async function authenticate(c: Context) {
-  const authenticateBodySchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(6),
-  })
+	const authenticateBodySchema = z.object({
+		email: z.string().email(),
+		password: z.string().min(6),
+	})
 
-  
-  try {
-    const { email, password } = authenticateBodySchema.parse(await c.req.json())
+	try {
+		const { email, password } = authenticateBodySchema.parse(await c.req.json())
 
-    const authenticateUseCase = makeAuthenticateUseCase({databaseConnectionString: c.env.DATABASE_URL})
+		const authenticateUseCase = makeAuthenticateUseCase({
+			databaseConnectionString: c.env.DATABASE_URL,
+		})
 
-    const {user} = await authenticateUseCase.execute({email, password})
+		const { user } = await authenticateUseCase.execute({ email, password })
 
-    return c.json({
-      user
-    })
-  } catch (error) {
-    if (error instanceof InvalidCredentialsError) {
-      return c.json({
-        message: "Invalid credentials"
-      }, 400)
-    }
+		return c.json({
+			user,
+		})
+	} catch (error) {
+		if (error instanceof InvalidCredentialsError) {
+			return c.json(
+				{
+					message: 'Invalid credentials',
+				},
+				400,
+			)
+		}
 
-    return c.json({
-      message: "Internal server error"
-    }, 500)
-  }
+		return c.json(
+			{
+				message: 'Internal server error',
+			},
+			500,
+		)
+	}
 }
