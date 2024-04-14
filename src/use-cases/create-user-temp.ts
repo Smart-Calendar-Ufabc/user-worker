d3974dba5e341b5ce802ef1505842bdd8a23ce7d
Giselle Hoekveld <gisellehoekveld@gmail.com>
2024-04-14 12:08:28 -0300

    Refactor CreateUserTempUseCase to use single quotes and format code for better readability

--- src/use-cases/create-user-temp.ts
+++ src/use-cases/create-user-temp.ts
@@ -1,42 +1,48 @@
-import { UserTemp } from "@prisma/client/edge";
-import { UsersTempRepository } from "../repositories/users-temp-repository";
-import { genSalt, hash } from "bcryptjs";
-import { generateSixDigitCode } from "../factories/generate-six-digit-code";
-import { UsersRepository } from "../repositories/users-repository";
-import { UserAlreadyExistsError } from "./errors/UserAlreadyExistsError";
+import { UserTemp } from '@prisma/client/edge'
+import { UsersTempRepository } from '../repositories/users-temp-repository'
+import { genSalt, hash } from 'bcryptjs'
+import { generateSixDigitCode } from '../factories/generate-six-digit-code'
+import { UsersRepository } from '../repositories/users-repository'
+import { UserAlreadyExistsError } from './errors/UserAlreadyExistsError'
 
 interface CreateUserTempRequest {
-  email: string;
-  password: string;
+	email: string
+	password: string
 }
 
 interface CreateUserTempResponse {
-  userTemp: UserTemp;
+	userTemp: UserTemp
 }
 
 export class CreateUserTempUseCase {
-  constructor(private usersTempRepository: UsersTempRepository, private usersRepository: UsersRepository) {}
+	constructor(
+		private usersTempRepository: UsersTempRepository,
+		private usersRepository: UsersRepository,
+	) {}
 
-  async execute({email, password}: CreateUserTempRequest): Promise<CreateUserTempResponse> {
-    const userWithSameEmail = await this.usersRepository.findUniqueByEmail(email);
+	async execute({
+		email,
+		password,
+	}: CreateUserTempRequest): Promise<CreateUserTempResponse> {
+		const userWithSameEmail =
+			await this.usersRepository.findUniqueByEmail(email)
 
-    if (userWithSameEmail) {
-      throw new UserAlreadyExistsError();
-    }
+		if (userWithSameEmail) {
+			throw new UserAlreadyExistsError()
+		}
 
-    const salt = await genSalt(10);
-    const code = generateSixDigitCode();
-    const userTemp = await this.usersTempRepository.create({
-      email,
-      password_hash: await hash(password, salt),
-      code,
-    });
+		const salt = await genSalt(10)
+		const code = generateSixDigitCode()
+		const userTemp = await this.usersTempRepository.create({
+			email,
+			password_hash: await hash(password, salt),
+			code,
+		})
 
-    // send email with code
-    console.log(`Sign-up code validate: ${code}`)
+		// send email with code
 
-    return {
-      userTemp
-    };
-  }
+		return {
+			userTemp,
+		}
+	}
 }
